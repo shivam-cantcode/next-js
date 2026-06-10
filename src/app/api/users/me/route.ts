3f59d099ddecaf44fd12029446c09261af217366
shivam yadav <yadav.shivam7389@gmail.com>
2026-06-10 15:59:04 +0530

    updated middlewares and me

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,7 +9,7 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDatafromToekn(request);
-    const user = await User.findById(userId).select("-password");
+    const user = await User.findById({ _id: userId }).select("-password ");
     return NextResponse.json({
       message: "User fetched successfully",
       success: true,
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Profile() {
   const router = useRouter();
@@ -17,9 +18,24 @@ export default function Profile() {
       toast.error(error.response?.data?.error || "Logout failed");
     }
   };
+
+  const [data, setData] = useState("nothing");
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.user._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
+      <h2 className="p-3 rounded-2xl bg-red-300 ">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <p>Profile page</p>
       <hr />
@@ -28,6 +44,12 @@ export default function Profile() {
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-900 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Getuser details
       </button>
     </div>
   );
