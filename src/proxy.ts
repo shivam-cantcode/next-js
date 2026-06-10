1a3749f8b66d503747acd77ae261d6fe282434bc
shivam yadav <yadav.shivam7389@gmail.com>
2026-06-10 15:05:29 +0530

    made changes in the file

---
+++ src/proxy.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value || "";
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup"],
+};
